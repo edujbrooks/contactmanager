544f4acde392376b6ca3c6470003dc81e4b772db
Edward Brooks Serrano <edubrooks@edubrooks.com>
2019-02-19 13:10:38 +0100

    Updates

---
+++ precache-manifest.8866c6f2b32c9752ccbbcd12558fa04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "09d4547027056b9b6339",
+    "url": "/contactmanager/static/js/main.09d45470.chunk.js"
+  },
+  {
+    "revision": "f8230af5e9cca04def7e",
+    "url": "/contactmanager/static/js/1.f8230af5.chunk.js"
+  },
+  {
+    "revision": "09d4547027056b9b6339",
+    "url": "/contactmanager/static/css/main.5a9b4f34.chunk.css"
+  },
+  {
+    "revision": "f8230af5e9cca04def7e",
+    "url": "/contactmanager/static/css/1.64856a0c.chunk.css"
+  },
+  {
+    "revision": "ec46266ebbd7aff1a284a0a24a9d0a11",
+    "url": "/contactmanager/index.html"
+  }
+];
